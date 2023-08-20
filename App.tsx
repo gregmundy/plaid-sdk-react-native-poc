0783a11d7c0dbcbd3db0feea5e374c5c1d607327
Greg Mundy <gregmundy@gmail.com>
2023-08-20 16:20:52 -0400

    chore: integrate plaid

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import PlaidLink, { LinkSuccess, LinkExit } from "react-native-plaid-link-sdk";
+
+// link-sandbox-ce7f78af-6562-4164-9e2e-22133a7117c7
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PlaidLink
+        tokenConfig={{ token: "link-sandbox-e9ba780d-2026-41e1-8647-6dffb39f46f8", noLoadingState: false }}
+        onSuccess={(success: LinkSuccess) => console.log(success)}
+        onExit={(exit: LinkExit) => console.log(exit)}
+      >
+        <Text>Add Account</Text>
+      </PlaidLink>
     </View>
   );
 }
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
